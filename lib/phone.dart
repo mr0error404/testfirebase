36cb2fae16edf30f1e5b269f94449bb20b5eb8c3
macbookpro <Eng.abdullahyseein@gmail.com>
2023-11-20 01:22:13 +0300

    firebase auth

--- lib/phone.dart
+++ lib/phone.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyPhone extends StatefulWidget {
@@ -13,48 +14,63 @@ class _MyPhoneState extends State<MyPhone> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    countryCode.text="+972";
+    countryCode.text = "+972";
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(left: 25,right: 25),
+        margin: EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(""
-                  "assets/img1.png",
+              Image.asset(
+                ""
+                "assets/img1.png",
                 width: 250,
                 height: 250,
               ),
               // Image.network("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.historichouses.org%2Fhouse%2Ffirle-place%2F&psig=AOvVaw2P4hq-yYU7QVyXmn-_fu1Y&ust=1697876168034000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDrieGXhIIDFQAAAAAdAAAAABAE"),
-              SizedBox(height: 25,),
-              Text("Phone Verification",
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Phone Verification",
                 style: TextStyle(
                   fontSize: 22,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 10,),
-              Text("We need to register your phonne before getting started",
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "We need to register your phonne before getting started",
                 style: TextStyle(
                   fontSize: 16,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 30,),
+              SizedBox(
+                height: 30,
+              ),
               Container(
                 height: 55,
                 decoration: BoxDecoration(
-                  border: Border.all(width: 1 ,color: Colors.grey,),
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Row(
                   children: [
-                    SizedBox(width: 20,),
+                    SizedBox(
+                      width: 20,
+                    ),
                     SizedBox(
                       width: 40,
                       child: TextField(
@@ -89,21 +105,30 @@ class _MyPhoneState extends State<MyPhone> {
                   ],
                 ),
               ),
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               SizedBox(
                 height: 45.0,
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: (){
-                    Navigator.pushNamed(context, "otp");
+                  onPressed: () async {
+                    await FirebaseAuth.instance.verifyPhoneNumber(
+                      phoneNumber: '+44 7123 123 456',
+                      verificationCompleted:
+                          (PhoneAuthCredential credential) {},
+                      verificationFailed: (FirebaseAuthException e) {},
+                      codeSent: (String verificationId, int? resendToken) {},
+                      codeAutoRetrievalTimeout: (String verificationId) {},
+                    );
+                    // Navigator.pushNamed(context, "otp");
                   },
                   child: Text("Send the code"),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green.shade600,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
-                      )
-                  ),
+                      )),
                 ),
               ),
             ],
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import firebase_analytics
+import firebase_auth
 import firebase_core
 import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
